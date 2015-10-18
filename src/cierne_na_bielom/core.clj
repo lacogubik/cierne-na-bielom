dd4c26caf378e3a16c93eec00177f01e32745c92
Laco Gubik <lacogubik@gmailcom>
2015-10-19 00:30:09 +0100

    Get basic book info

--- src/cierne_na_bielom/core.clj
+++ src/cierne_na_bielom/core.clj
@@ -1,5 +1,18 @@
 (ns cierne-na-bielom.core
-  (:gen-class))
+  (:gen-class)
+  (:require [reaver :refer [parse extract-from text attr]]))
+
+
+(def url "http://www.ciernenabielom.sk/uvod/strana-2/")
+
+
+(defn get-book
+  []
+  (-> url
+      slurp
+      parse
+      (extract-from ".foot-line" [:name :url :img] "h2 a.nazov" text "h2 a.nazov" (attr :href) ".foot-img-cont a" (attr :href))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
